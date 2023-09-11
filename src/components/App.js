17f6731a26ef8deeae0b8c84f51f05d05a4c2514
sohdonald <sohdonald@donaldUbuntu.myguest.virtualbox.org>
2023-09-11 10:32:31 -0400

    got images to display

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,23 @@
 // create your App component here
+import React, {useState, useEffect} from "react";
+
+function App() {
+    const [dogImg, setDogImg] = useState([]);
+
+    useEffect(() => {
+        fetch(`https://dog.ceo/api/breeds/image/random`)
+        .then((r) =>r.json())
+        .then((data) => {
+            setDogImg(data.message)
+        })
+    })
+
+    return(
+        <>
+        <p>Loading...</p>
+        <img src={dogImg} alt="A Random Dog"></img>
+        </>
+    );
+}
+
+export default App;
